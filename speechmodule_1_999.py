5e7e541663a89b41f58c4b858a246dcaf253354c
dfi <themaildde@gmail.com>
2013-07-08 00:14:45 +0800

    Create speechmodule_1_999.py
    1

---
+++ speechmodule_1_999.py
@@ -0,0 +1,36 @@
+z = range(1000)
+
+a = range(10)
+b = range(10, 20)
+c = range(20, 100, 10)
+d = range(100, 1000, 100)
+e = [i for i in range(21, 100) if i % 10 != 0]
+f = [i for i in range(100, 1000) if ( i % 100 in range(1, 10) ) ]
+g = [i for i in range(100, 1000) if ( i % 100 in range(10, 20) ) ]
+h = [i for i in range(120, 1000) if (i % 10 == 0 and i % 100 != 0)]
+
+x = []
+
+for i in z:
+    if i not in a:
+        if i not in b:
+            if i not in c:
+                if i not in d:
+                    if i not in e:
+                        if i not in f:
+                            if i not in g:
+                                if i not in h:
+                                    x.append(i)
+        
+print 'a:\n', a
+print 'b:\n', b
+print 'c:\n', c
+print 'd:\n', d
+print 'e:\n', e
+print 'f:\n', f
+print 'g:\n', g
+print 'h:\n', h
+
+print 'x:\n', x
+print '--------------------'
+print len(x)
